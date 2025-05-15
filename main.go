3fcaa8401dbbe1ec6d4405008d4f89d3e2e4d7f6
karl doherty <karldoherty15@icloud.com>
2025-05-15 22:34:34 +0100

    added health command to check server status

--- main.go
+++ main.go
@@ -2,18 +2,31 @@ package main
 
 // go standard library
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
+type apiHandler struct{}
+
 func main() {
+	const filePathRoot = "."
+	const port = "8080"
+
 	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	serveMux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filePathRoot))))
+	serveMux.HandleFunc("/healthz/", healthzHandler)
+
 	server := http.Server{
 		Handler: serveMux,
-		Addr:    ":8080",
+		Addr:    ":" + port,
 	}
-	if err := server.ListenAndServe(); err != nil {
-		fmt.Println(err.Error())
-	}
+
+	log.Printf("Serving files from %v and listening on port %v", filePathRoot, port)
+	log.Fatal(server.ListenAndServe())
+}
+
+func healthzHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
