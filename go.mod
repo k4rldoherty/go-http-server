4a146765b39df0dd6d7862a3fa8fca93bbea2dcf
karl doherty <karldoherty15@icloud.com>
2025-05-15 15:46:22 +0100

    initial server setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/k4rldoherty/go-http-server
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+// go standard library
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	serveMux := http.NewServeMux()
+	server := http.Server{
+		Handler: serveMux,
+		Addr:    ":8080",
+	}
+	if err := server.ListenAndServe(); err != nil {
+		fmt.Println(err.Error())
+	}
+}
